5de77837becfac82d61dee7a52e58a52be763d0c
JJ Iula <jonathaniula5@gmail.com>
2024-02-13 13:44:19 -0500

    progress

---
+++ vue/src/components/CampaignsList.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="container" v-for="campaign in campaigns" v-bind:key="campaign.campaign_id">
+      <div @click="toggleShowDetails(campaign)">
+        <div>
+          {{ campaign.username }}
+        </div>
+        <div>
+          {{ campaign.campaignName}}
+        </div>
+        <div>
+          {{ campaign.campaignType }}
+        </div>
+        <div>
+          {{ campaign.description }}
+        </div>
+        <div>
+          {{ campaign.amountGoal }}
+        </div>
+        <div>
+          {{ campaign.balance }}
+        </div>
+      </div>
+    </div>
+</template>
+<script>
+export default {
+  name: 'campaign-list',
+  props: ['campaigns'],
+  methods: {
+    toggleShowDetails(campaign){
+      this.$store.commit('TOGGLE_SHOW_DETAILS', campaign);
+    }
+  }
+};
+</script>
