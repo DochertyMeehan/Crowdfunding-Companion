7566f7a68b2275a7fd1fcb1c376d75a664227652
Anthony Cicero <acicero73@gmail.com>
2024-02-21 15:46:11 -0500

    push

--- java/src/main/java/com/techelevator/model/DonorUserDto.java
+++ java/src/main/java/com/techelevator/model/DonorUserDto.java
@@ -4,11 +4,19 @@ public class DonorUserDto {
 
     private String username;
     private double amount;
+    private String donation_comment;
 
-    public DonorUserDto(String username, double amount) {
+    public DonorUserDto(String username, double amount, String donation_comment) {
+        this.username = username;
+        this.amount = amount;
+        this.donation_comment = donation_comment;
+    }
+
+    public DonorUserDto(String username, double amount){
         this.username = username;
         this.amount = amount;
     }
+
 
     public String getUsername() {
         return username;
@@ -24,5 +32,13 @@ public class DonorUserDto {
 
     public void setAmount(double amount) {
         this.amount = amount;
+    }
+
+    public String getDonation_comment() {
+        return donation_comment;
+    }
+
+    public void setDonation_comment(String donation_comment) {
+        this.donation_comment = donation_comment;
     }
 }
